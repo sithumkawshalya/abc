1208ff7fd9f5d41dd10ca4fb5135f32c30f55bd2
sithumkawshalya <hmgkskgamage@gmail.com>
2025-10-04 10:29:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=040438fb8ccdb28a' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=04045735ecb9b0d1
+' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
